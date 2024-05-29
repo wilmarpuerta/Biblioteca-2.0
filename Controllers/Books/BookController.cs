8da77deae7b9d0c6ad0626e924171b12cdedcae0
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-28 21:05:20 -0500

    Creación de controladores de libros

---
+++ Controllers/Books/BookController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca_2._0.Controllers.Books
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        
+    }
+}
---
+++ Controllers/Books/BookUpdateController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca_2._0.Controllers.Books
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookUpdateController : ControllerBase
+    {
+        
+    }
+}
