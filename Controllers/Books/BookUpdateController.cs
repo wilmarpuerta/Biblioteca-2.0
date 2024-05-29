6fdda8e9edd7d4820ea44be1a4ed3ec1b325dde6
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-29 04:29:23 -0500

    Actualización de libros (PUT)

--- Controllers/Books/BookUpdateController.cs
+++ Controllers/Books/BookUpdateController.cs
@@ -1,7 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Biblioteca_2._0.Models;
+using Biblioteca_2._0.Services.Books;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca_2._0.Controllers.Books
@@ -10,6 +10,27 @@ namespace Biblioteca_2._0.Controllers.Books
     [Route("api/[controller]")]
     public class BookUpdateController : ControllerBase
     {
-        
+        private readonly IBookRepository _bookRepository;
+
+        public BookUpdateController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpPut("Actualizar/{id}")]
+        public IActionResult BookUpdate(int id, [FromBody] Book book)
+        {
+            var bookUpdate = _bookRepository.GetBookById(id);
+
+            bookUpdate.Title = book.Title;
+            bookUpdate.Pages = book.Pages;
+            bookUpdate.Language = book.Language;
+            bookUpdate.PublicationDate = book.PublicationDate;
+            bookUpdate.Description = book.Description;
+
+            _bookRepository.UpdateBook(id, book);
+            
+            return Ok("Libro actualizado");
+        }
     }
 }
