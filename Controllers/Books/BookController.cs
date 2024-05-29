4b163eee5a32af3198053bfa0e7435b7ac4a41d8
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-29 04:27:48 -0500

    Controlador de libros (GET)

--- Controllers/Books/BookController.cs
+++ Controllers/Books/BookController.cs
@@ -1,4 +1,6 @@
 
+using Biblioteca_2._0.Models;
+using Biblioteca_2._0.Services.Books;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca_2._0.Controllers.Books
@@ -7,6 +9,23 @@ namespace Biblioteca_2._0.Controllers.Books
     [Route("api/[controller]")]
     public class BookController : ControllerBase
     {
-        
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet("listar")]
+        public IEnumerable<Book> GetBooksAll()
+        {
+            return _bookRepository.GetBooks();
+        }
+
+        [HttpGet("listar/{id}")]
+        public Book GetBookById(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
     }
 }
