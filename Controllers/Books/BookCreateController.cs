51ccb5c2b67f49ccf92f8720e5b9404603bc7d41
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-29 04:28:28 -0500

    Creación de libros (POST)

--- Controllers/Books/BookCreateController.cs
+++ Controllers/Books/BookCreateController.cs
@@ -1,4 +1,6 @@
 
+using Biblioteca_2._0.Models;
+using Biblioteca_2._0.Services.Books;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca_2._0.Controllers.Books
@@ -7,6 +9,16 @@ namespace Biblioteca_2._0.Controllers.Books
     [Route("api/[controller]")]
     public class BookCreateController : ControllerBase
     {
-        
+        private readonly IBookRepository _bookRepository;
+        public BookCreateController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        [HttpPost("Crear")]
+        public IActionResult Create([FromBody] Book book)
+        {
+            _bookRepository.AddBook(book);
+            return Ok("El libro se creo exitosamente");
+        }
     }
 }
