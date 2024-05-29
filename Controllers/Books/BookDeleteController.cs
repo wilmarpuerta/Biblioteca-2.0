0873e9cb1ecc342d3aca287a7e350c770f818ba8
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-29 04:28:54 -0500

    Eliminación de libros (DELETE)

--- Controllers/Books/BookDeleteController.cs
+++ Controllers/Books/BookDeleteController.cs
@@ -1,4 +1,5 @@
 
+using Biblioteca_2._0.Services.Books;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca_2._0.Controllers.Books
@@ -7,6 +8,18 @@ namespace Biblioteca_2._0.Controllers.Books
     [Route("api/[controller]")]
     public class BookDeleteController : ControllerBase
     {
-        
+        private readonly IBookRepository _bookRepository;
+
+        public BookDeleteController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpDelete("Eliminar/{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            _bookRepository.DeleteBook(id);
+            return Ok("Libro eliminado con exito");
+        }
     }
 }
